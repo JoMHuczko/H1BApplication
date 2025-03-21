950a320bee9acd4f1c18c5702c8b60c140182c1c
jhuczko <jhuczko@alumni.cmu.edu>
2025-03-21 14:14:25 -0400

    Removed unused pages (the contact info and about page) because I do not want to implement them yet.

--- app.py
+++ app.py
@@ -1,26 +1,22 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
 # Configure SQLite database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clients.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["MONGO_URI"] = "mongodb://atlas-sql-67dc87ea8fffd152f43953a3-9hxok.a.query.mongodb.net/sample_mflix?ssl=true&authSource=admin"
+mongo = PyMongo(app)
 
-# Initialize the database
-db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    # Example of querying the database
+    users = mongo.db.users.find()
+    return str(users)
 
 @app.route("/")
 def home():
     return render_template("home.html")
-
-@app.route("/about")
-def about():
-    return "This is the About Page"
-
-@app.route("/contact")
-def contact():
-    return "This will have my contact information"
 
 @app.route('/test')
 def test():
